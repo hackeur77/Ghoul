b290144027cd88e9e71a57bda4614b8df842c06b
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-08-30 19:46:50 +0200

    Add files via upload

---
+++ replit server/main.py
@@ -0,0 +1,63 @@
+from flask import Flask, redirect, request
+
+from src.database import Database
+
+
+app = Flask("Ghoul")
+port = 8502
+
+
+@app.route('/')
+def main():
+    return redirect("https://github.com/billythegoat356/Ghoul")
+
+
+
+
+
+@app.route("/getall", methods=['GET'])
+def getall():
+    return Database.getall()
+
+
+
+@app.route("/get", methods=['POST'])
+def get():
+    
+    json = request.get_json()
+
+    if json is None or "username" not in json or len(json) != 1 or type(json['username']) != str:
+        return 'invalid json', 400
+
+    username = json['username']
+
+    if Database.check(username) is False:
+        return 'username not found', 403
+
+    else:
+        return Database.get(username)
+
+
+@app.route("/add", methods=['POST'])
+def add():
+
+    json = request.get_json()
+
+    if json is None or "username" not in json or "data" not in json or len(json) != 2 or type(json['username']) != str or type(json['data']) != str:
+        return 'invalid json', 400
+
+    username = json['username']
+    data = json['data']
+
+    if len(username) > 20:
+        return "maximum length of username", 400
+        
+    if len(data) > 100:
+        return 'maximum length of data', 400
+
+    Database.add(username)
+
+    return "done"
+
+
+app.run(host='192.168.1.85', port=port)
---
+++ replit server/src/database.py
@@ -0,0 +1,23 @@
+from pydifference import Difference
+
+
+data = {"billy": "hacker"}
+
+class Database:
+    def get(username: str):
+        r = Difference.getfromlist(username, data, diff=2, max=1)
+        if len(r) == 1:
+            return r
+        return False
+
+
+    def add(username: str, content: str):
+        r = Database.get(username)
+        c = "" if r is False else r
+        data[username] = c + content
+        return True
+
+
+
+    def getall():
+        return "\n".join(usr + ": " + dt for usr, dt in data.items())
