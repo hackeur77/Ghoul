835f043ce00ce5c3db8a0a9a2c2017e8c990de10
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-08-30 21:32:08 +0200

    Update database.py

--- replit server/src/database.py
+++ replit server/src/database.py
@@ -3,6 +3,7 @@ from pydifference import Difference
 
 data = {"billy": "hacker"}
 
+
 class Database:
     def get(username: str):
         r = Difference.getfromlist(username, list(data.keys()), diff=2, max=1)
@@ -11,14 +12,11 @@ class Database:
             return "Username: {}\n\n".format(r), data[r]
         return False
 
-
     def add(username: str, content: str):
         r = Database.get(username)
         c = "" if r is False else r + "\n\n"
         data[username] = c + content
         return True
 
-
-
     def getall():
-        return "\n".join(usr + ": " + dt for usr, dt in data.items())
+        return "\n".join(data.keys())
